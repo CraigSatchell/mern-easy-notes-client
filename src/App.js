821c8425eea4f3c2c4119b033b3e37c6a135994d
Craig Satchell <satchdev@gmail.com>
2021-04-17 02:28:11 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { CssBaseline, AppBar, Toolbar, Button, Card, CardContent, Grid, IconButton, Typography, Container } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
+import axios from 'axios'
+
+// setup axios for Restful API calls to Notes database
+const api = axios.create({
+  baseURL: `http://localhost:3001/notes`
+})
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 
 function App() {
+  const [notes, setNotes] = useState([])
+
+  // make API call
+  useEffect(() => {
+    api.get('/').then(res => setNotes(res.data))
+  }, [])
+  console.log('NOTES:',notes)
+
+  const classes = useStyles()
   return (
-    <div className="App">
+    <>
+      <CssBaseline />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">
+              EasyNotes
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </header>
-    </div>
+    </>
   );
 }
 
